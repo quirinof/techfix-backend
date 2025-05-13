c17e6d8160f7232de2a6847f41a3f0cb26adca73
HalysonSA <halysonsa@gmail.com>
2025-05-12 23:00:46 -0300

    fix: return exception

--- src/controllers/customer/find-by-id.ts
+++ src/controllers/customer/find-by-id.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 import { makeFindCustomerByIdService } from "../../services/factories/customer/make-find-customer-by-id";
 
 export async function findById(req: Request, res: Response) {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const findCustomerByIdService = makeFindCustomerByIdService();
+    const findCustomerByIdService = makeFindCustomerByIdService();
 
-  const { customer } = await findCustomerByIdService.handle(Number(id));
+    const { customer } = await findCustomerByIdService.handle(Number(id));
 
-  res.status(200).json({ customer });
-};
+    res.status(200).json({ customer });
+  } catch (err) {
+    res.status(404).send();
+  }
+}
