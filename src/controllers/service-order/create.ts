f480b564bf7b92c087d765b49f571b86d0519ce8
quirinof <matheusquinoff@gmail.com>
2025-05-26 09:35:52 -0300

    refactor: changes service SO in customer connect #38

--- src/controllers/service-order/create.ts
+++ src/controllers/service-order/create.ts
@@ -2,13 +2,12 @@ import { Request, Response } from "express";
 import { makeCreateServiceOrderService } from "../../services/factories/service-order/make-create-service-order";
 
 export async function create(req: Request, res: Response) {
-  const { description, estimate, customer } = req.body;
-
+  const { description, estimate, customerId } = req.body;
   const createServiceOrderService = makeCreateServiceOrderService();
   const { serviceOrder } = await createServiceOrderService.handle({
     description,
     estimate,
-    customer,
+    customerId,
   });
   res.status(201).json({ serviceOrder });
 }
