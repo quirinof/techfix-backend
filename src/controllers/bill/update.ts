d4a5a34a2f6f3576cccb8dc5fabcb963ef47a9b2
quirinof <matheusquinoff@gmail.com>
2025-06-01 20:21:39 -0300

    feat: add update bill controller #51

---
+++ src/controllers/bill/update.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { makeUpdateBillService } from "../../services/factories/bill/make-update-bill";
+import { updateBillSchema } from "../../schemas/bill-schema";
+import { zodErrorFormatter } from "../../utils/zod-error-formatter";
+
+export async function update(req: Request, res: Response) {
+  const parseResult = updateBillSchema.safeParse(req.body);
+
+  if (!parseResult.success) {
+    const formattedErrors = zodErrorFormatter(parseResult.error);
+    res.status(400).json(formattedErrors);
+    return;
+  }
+
+  const data = parseResult.data;
+  const dueDate =
+    typeof data.dueDate === "string" ? new Date(data.dueDate) : data.dueDate;
+
+  const updateBillService = makeUpdateBillService();
+
+  const { bill } = await updateBillService.handle({
+    ...data,
+    id: Number(req.params.id),
+    dueDate,
+  });
+
+  res.status(200).json({ bill });
+}
